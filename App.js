4e13c115aa82ee7b9a1a1deaf5cc3800dad38711
toggiuu <106525817+toggiuu@users.noreply.github.com>
2024-05-09 21:58:55 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './components/Game';
+import './style.css';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Tic Tac Toe</h1>
+            <Game />
+        </div>
+    );
+}
+
+export default App;
