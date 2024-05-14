356e7ce95782e02cb0acfd9c5f1db134ab66fd24
toggiuu <106525817+toggiuu@users.noreply.github.com>
2024-05-14 23:38:46 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Game from './components/Game';
 import './style.css';
+import backgroundImage from './wood.jpg'; // 이미지 파일을 import
 
 function App() {
     return (
-        <div className="app">
+        <div className="app" style={{ backgroundImage: `url(${backgroundImage})` }}>
             <h1>Tic Tac Toe</h1>
             <Game />
         </div>
