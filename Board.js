0cd0b642cb39f3e910d088fe1363601ce8b5d53a
toggiuu <106525817+toggiuu@users.noreply.github.com>
2024-05-17 19:24:00 +0900

    Update Board.js

--- Board.js
+++ Board.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Square from './Square';
 
-function Board({ squares, onClick }) {
+function Board({ squares, onClick, winnerLine }) {
+    const renderSquare = (i) => {
+        const highlight = winnerLine && winnerLine.includes(i);
+        return (
+            <Square 
+                key={i} 
+                value={squares[i]} 
+                onClick={() => onClick(i)}
+                highlight={highlight} 
+            />
+        );
+    };
+
     return (
         <div className="board">
-            {squares.map((square, i) => (
-                <Square key={i} value={square} onClick={() => onClick(i)} />
-            ))}
+            {squares.map((square, i) => renderSquare(i))}
         </div>
     );
 }
+
 export default Board;
